27416edfa480b81e305cb97c7e9307666f5aa277
beyerja <beyerja@gmx.de>
2021-08-20 11:31:04 +0200

    Use the right coefficients in correlation plotting
    - Use the right coefficients in correlation plotting (actual TGC 
    coefficients instead of just deviation).

--- py/ROOTChecks/CheckWWTGCMuAccCorrelations.py
+++ py/ROOTChecks/CheckWWTGCMuAccCorrelations.py
@@ -26,22 +26,20 @@ def ratio(a,b):
   """
   return np.where(np.abs(b)>0.01,a/b,a)
 
-def TGC_gradiant(y_TGC, y_SM, dev_scale, n_MC, SM_xsection, lumi):
-  """ Calculate the gradient wrt. to the TGC at the SM point.
+def TGC_lin_coef(y_TGC, y_SM, dev_scale):
+  """ Calculate the normalised gradient wrt. to the TGC at the SM point.
       y_TGC represents the distribution for a change of dev_scale in one TGC.
       Assumes only linear dependence on the TGC.
   """
-  return y_SM * SM_xsection / (n_MC * dev_scale) * (ratio(y_TGC,y_SM)-1.0)
+  return (ratio(y_TGC,y_SM)-1.0) / dev_scale
 
 
-def add_derivative_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale,
-                        SM_xsection, lumi):
+def add_derivative_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale):
   """ Add the plots showing the derivative of the cross section in each bin wrt.
       the TGC.
   """
   n_MC = np.sum(xy_dict["no cut"]["SM"][1])
-  TGC_grad = functools.partial(TGC_gradiant, dev_scale=dev_scale, n_MC=n_MC, 
-                                   SM_xsection=SM_xsection, lumi=lumi)
+  TGC_grad = functools.partial(TGC_lin_coef, dev_scale=dev_scale)
   
   x = xy_dict["no cut"]["SM"][0]
   d_P_nocut = TGC_grad(xy_dict["no cut"][TGC_name][1], xy_dict["no cut"]["SM"][1])
@@ -54,14 +52,12 @@ def add_derivative_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale,
   ax.hist(x, weights=d_P_dc, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step', label="$\Delta c += \delta_{cut}$")
   ax.hist(x, weights=d_P_dw, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step', label="$\Delta w += \delta_{cut}$")
 
-def add_rel_change_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale,
-                        SM_xsection, lumi):
+def add_rel_change_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale):
   """ Add the plots showing the relative change from the SM for different cut 
       values.
   """
   n_MC = np.sum(xy_dict["no cut"]["SM"][1])
-  TGC_grad = functools.partial(TGC_gradiant, dev_scale=dev_scale, n_MC=n_MC, 
-                               SM_xsection=SM_xsection, lumi=lumi)
+  TGC_grad = functools.partial(TGC_lin_coef, dev_scale=dev_scale)
 
   x = xy_dict["no cut"]["SM"][0]
   grad_P_nocut = TGC_grad(xy_dict["no cut"][TGC_name][1], xy_dict["no cut"]["SM"][1])
@@ -81,8 +77,7 @@ def add_rel_change_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale,
 
   
 def create_reweighting_plot(xy_dict, output_base, obs_name, obs_range, 
-                            lumi, SM_xsection, dev_scale, cut_dev,
-                            output_formats=["pdf","png"]):
+                            dev_scale, cut_dev, output_formats=["pdf","png"]):
   """ Create the reweighting plot for one observable.
   """
   eM_chi, eP_chi = PN.chirality_str(IFH.find_chirality(output_base))
@@ -95,17 +90,15 @@ def create_reweighting_plot(xy_dict, output_base, obs_name, obs_range,
     hist_range=(obs_range[1],obs_range[2])
     
     # Upper plot
-    add_derivative_plot(ax_up, TGC_name, xy_dict, n_bins, hist_range, dev_scale,
-                        SM_xsection, lumi)
+    add_derivative_plot(ax_up, TGC_name, xy_dict, n_bins, hist_range, dev_scale)
     
-    ax_up.set_ylabel(r"$\frac{{d\sigma^{{bin}}}}{{d {}}} [\frac{{fb}}{{1}}]$".format(TGC_name_map[TGC_name]), fontsize=30)
+    ax_up.set_ylabel(r"$\frac{{1}}{{\sigma^{{bin}}}}\frac{{d\sigma^{{bin}}}}{{d {}}} [\frac{{fb}}{{1}}]$".format(TGC_name_map[TGC_name]), fontsize=30)
     ax_up.set_xlim(hist_range)
 
     ax_up.legend(fontsize=16, title="${}{}$, $\delta_{{cut}}={}$".format(eM_chi, eP_chi, dev_scale, cut_dev), title_fontsize=16)
     
     # Lower plot
-    add_rel_change_plot(ax_down, TGC_name, xy_dict, n_bins, hist_range, dev_scale,
-                        SM_xsection, lumi)  
+    add_rel_change_plot(ax_down, TGC_name, xy_dict, n_bins, hist_range, dev_scale)  
     ax_down.set_ylabel(r"$\frac{y_{cut}-y_{nocut}}{y_{nocut}}$ [%]")
     ax_down.set_xlabel(obs_name)
     
@@ -119,11 +112,11 @@ def create_reweighting_plot(xy_dict, output_base, obs_name, obs_range,
     plt.close(fig)
 
 def check_reweighting(root_file, tgc_config_path, tgc_point_path, output_dir,
-                      observables, lumi, tree_name = "WWObservables"):
+                      observables, tree_name = "WWObservables"):
   """ Check what reweighting does to the observable distributions for the given 
       events in the ROOT file.
   """
-  rdf = ROOT.RDataFrame(tree_name, root_file).Filter("rescan_weights.weight1 > 0.01")
+  rdf = ROOT.RDataFrame(tree_name, root_file).Range(0,300000).Filter("rescan_weights.weight1 > 0.01")
   
   # Muon Acceptance related setup
   cut_val = 0.9925 # 7deg
@@ -174,9 +167,6 @@ def check_reweighting(root_file, tgc_config_path, tgc_point_path, output_dir,
         hist_dict[obs_name][cut_name][point] = cut_rdf.Histo1D(h_def, obs_name, 
                                                                weight_name)
     
-  # Get the SM cross section, needed for correct uncertainty weighting
-  SM_xsection = rdf.Mean("cross_section")
-      
   # Make the plots for each observable
   for obs_name, obs_range in observables.items():
     # First transform hists to arrays for easier matplotlib plotting
@@ -185,8 +175,8 @@ def check_reweighting(root_file, tgc_config_path, tgc_point_path, output_dir,
       xy_dict[cut_name] = {}
       for point, hist in cut_dict.items():
         xy_dict[cut_name][point] = PRHH.TH1_to_arrays(hist) 
-    create_reweighting_plot(xy_dict, output_base, obs_name, obs_range, lumi, 
-                            SM_xsection.GetValue(), dev_scale, cut_dev)
+    create_reweighting_plot(xy_dict, output_base, obs_name, obs_range, 
+                            dev_scale, cut_dev)
       
 def main():
   """ Create check plots that investigate how the weights in the given ROOT file
@@ -206,13 +196,11 @@ def main():
                   "costh_l_star" :      (30, -1., 1.),
                   "phi_l_star":         (30, -np.pi, np.pi) }
   
-  lumi = 1000 # 2ab^-1 total ~> 1ab^-1 for each of the two allowed chiral states
-  
   RL_path = "/nfs/dust/ilc/group/ild/beyerjac/TGCAnalysis/SampleProduction/NewMCProduction/4f_WW_sl/4f_WW_sl_eR_pL.root"
   LR_path = "/nfs/dust/ilc/group/ild/beyerjac/TGCAnalysis/SampleProduction/NewMCProduction/4f_WW_sl/4f_WW_sl_eL_pR.root"
   
-  check_reweighting(RL_path, tgc_config_path, tgc_point_path, output_dir, observables, lumi)
-  check_reweighting(LR_path, tgc_config_path, tgc_point_path, output_dir, observables, lumi)
+  check_reweighting(RL_path, tgc_config_path, tgc_point_path, output_dir, observables)
+  check_reweighting(LR_path, tgc_config_path, tgc_point_path, output_dir, observables)
   
 
 if __name__ == "__main__":
