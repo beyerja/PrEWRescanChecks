780f371091b5d4ffc3d5aa61011dc00b35d07028
beyerja <beyerja@gmx.de>
2021-08-25 16:15:45 +0200

    Add cut derivative WW plots
    - Add plots for the TGC-mu acc. correlations in WW that also show the 
    cut derivative.

--- py/ROOTChecks/CheckWWTGCMuAccCorrelations.py
+++ py/ROOTChecks/CheckWWTGCMuAccCorrelations.py
@@ -22,47 +22,51 @@ TGC_name_map = {
   'la' : r"\lambda_{{\gamma}}"
 }
 
+cut_name_map = {
+  'c_shift': r"\Delta c",
+  'w_shift': r"\Delta w"
+}
+
 def ratio(a,b):
   """ Calculate ratio between two arrays, if denominator point is 0 set ratio=1.
   """
   return np.where(np.abs(b)>0.01,a/b,1.0)
 
-def TGC_lin_coef(y_TGC, y_SM, dev_scale):
-  """ Calculate the normalised gradient wrt. to the TGC at the SM point.
-      y_TGC represents the distribution for a change of dev_scale in one TGC.
-      Assumes only linear dependence on the TGC.
+def lin_coef(y_delta, y_zero, delta):
+  """ Calculate the normalised gradient:
+        1/y_zero * (y_delta-y_zero)/delta
   """
-  return (ratio(y_TGC,y_SM)-1.0) / dev_scale
+  return (ratio(y_delta,y_zero)-1.0) / delta
 
 
-def add_derivative_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale):
+def add_TGC_derivative_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale):
   """ Add the plots showing the derivative of the cross section in each bin wrt.
       the TGC.
   """
-  TGC_grad = functools.partial(TGC_lin_coef, dev_scale=dev_scale)
+  grad = functools.partial(lin_coef, dev_scale=dev_scale)
   
   x = xy_dict["no cut"]["SM"][0]
-  d_P_nocut = TGC_grad(xy_dict["no cut"][TGC_name][1], xy_dict["no cut"]["SM"][1])
-  d_P_cut = TGC_grad(xy_dict["cut"][TGC_name][1], xy_dict["cut"]["SM"][1])
-  d_P_dc = TGC_grad(xy_dict["c shift"][TGC_name][1], xy_dict["c shift"]["SM"][1])
-  d_P_dw = TGC_grad(xy_dict["w shift"][TGC_name][1], xy_dict["w shift"]["SM"][1])
+  d_P_nocut = grad(xy_dict["no cut"][TGC_name][1], xy_dict["no cut"]["SM"][1])
+  d_P_cut = grad(xy_dict["cut"][TGC_name][1], xy_dict["cut"]["SM"][1])
+  d_P_dc = grad(xy_dict["c_shift"][TGC_name][1], xy_dict["c_shift"]["SM"][1])
+  d_P_dw = grad(xy_dict["w_shift"][TGC_name][1], xy_dict["w_shift"]["SM"][1])
   
   ax.hist(x, weights=d_P_nocut, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step', label="no cut")
   ax.hist(x, weights=d_P_cut, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step', label="7deg-cut")
   ax.hist(x, weights=d_P_dc, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step', label="$\Delta c $+=$ \delta_{cut}$")
   ax.hist(x, weights=d_P_dw, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step', label="$\Delta w $+=$ \delta_{cut}$")
 
-def add_diff_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale):
+def add_TGC_diff_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale):
   """ Add the plots showing the relative change from the SM for different cut 
       values.
   """
-  TGC_grad = functools.partial(TGC_lin_coef, dev_scale=dev_scale)
+  grad = functools.partial(lin_coef, dev_scale=dev_scale)
 
   x = xy_dict["no cut"]["SM"][0]
-  grad_P_nocut = TGC_grad(xy_dict["no cut"][TGC_name][1], xy_dict["no cut"]["SM"][1])
-  grad_P_cut = TGC_grad(xy_dict["cut"][TGC_name][1], xy_dict["cut"]["SM"][1])
-  grad_P_dc = TGC_grad(xy_dict["c shift"][TGC_name][1], xy_dict["c shift"]["SM"][1])
-  grad_P_dw = TGC_grad(xy_dict["w shift"][TGC_name][1], xy_dict["w shift"]["SM"][1])
+  grad_P_nocut = grad(xy_dict["no cut"][TGC_name][1], xy_dict["no cut"]["SM"][1])
+  grad_P_cut = grad(xy_dict["cut"][TGC_name][1], xy_dict["cut"]["SM"][1])
+  grad_P_dc = grad(xy_dict["c_shift"][TGC_name][1], xy_dict["c_shift"]["SM"][1])
+  grad_P_dw = grad(xy_dict["w_shift"][TGC_name][1], xy_dict["w_shift"]["SM"][1])
 
   diff_P_cut = grad_P_cut - grad_P_nocut
   diff_P_dc = grad_P_dc - grad_P_nocut
@@ -74,10 +78,49 @@ def add_diff_plot(ax, TGC_name, xy_dict, n_bins, hist_range, dev_scale):
   ax.hist(x, weights=diff_P_dc*scale, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step')
   ax.hist(x, weights=diff_P_dw*scale, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step')
 
+def add_cut_derivative_plot(ax, cut_name, xy_dict, n_bins, hist_range, dev_scale):
+  """ Add the plots showing the derivative of the cross section in each bin wrt.
+      the muon acceptance parameter shift.
+  """
+  grad = functools.partial(lin_coef, dev_scale=dev_scale)
   
-def create_reweighting_plot(xy_dict, output_base, obs_name, obs_range, 
-                            dev_scale, cut_dev, process_str,
-                            output_formats=["pdf","png"]):
+  x = xy_dict["no cut"]["SM"][0]
+  d_P_SM = grad(xy_dict[cut_name]["SM"][1], xy_dict["cut"]["SM"][1])
+  d_P_g1z = grad(xy_dict[cut_name]["g1z"][1], xy_dict["cut"]["g1z"][1])
+  d_P_ka = grad(xy_dict[cut_name]["ka"][1], xy_dict["cut"]["ka"][1])
+  d_P_la = grad(xy_dict[cut_name]["la"][1], xy_dict["cut"]["la"][1])
+  
+  ax.hist(x, weights=d_P_SM, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step', label="SM")
+  ax.hist(x, weights=d_P_g1z, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step', label="$g_{1}^{Z}$+=$ \delta_{TGC}$")
+  ax.hist(x, weights=d_P_ka, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step', label="$\kappa_{\gamma}$+=$ \delta_{TGC}$")
+  ax.hist(x, weights=d_P_la, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step', label="$\lambda_{\gamma}$+=$ \delta_{TGC}$")
+
+def add_cut_diff_plot(ax, cut_name, xy_dict, n_bins, hist_range, dev_scale):
+  """ Add the plots showing the relative change from the base cut for different 
+      TGC values.
+  """
+  grad = functools.partial(lin_coef, dev_scale=dev_scale)
+
+  x = xy_dict["no cut"]["SM"][0]
+  grad_P_SM = grad(xy_dict[cut_name]["SM"][1], xy_dict["cut"]["SM"][1])
+  grad_P_g1z = grad(xy_dict[cut_name]["g1z"][1], xy_dict["cut"]["g1z"][1])
+  grad_P_ka = grad(xy_dict[cut_name]["ka"][1], xy_dict["cut"]["ka"][1])
+  grad_P_la = grad(xy_dict[cut_name]["la"][1], xy_dict["cut"]["la"][1])
+
+  diff_P_g1z = grad_P_g1z - grad_P_SM
+  diff_P_ka = grad_P_ka - grad_P_SM
+  diff_P_la = grad_P_la - grad_P_SM
+
+  scale=1.0
+  ax.hist([], alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step')
+  ax.hist(x, weights=diff_P_g1z*scale, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step')
+  ax.hist(x, weights=diff_P_ka*scale, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step')
+  ax.hist(x, weights=diff_P_la*scale, alpha=0.9, lw=3, range=hist_range, bins=n_bins, histtype=u'step')
+
+  
+def create_reweighting_plots(xy_dict, output_base, obs_name, obs_range, 
+                             dev_scale, cut_dev, process_str,
+                             output_formats=["pdf","png"]):
   """ Create the reweighting plot for one observable.
   """
   for TGC_name in ["g1z", "ka", "la"]:
@@ -88,15 +131,15 @@ def create_reweighting_plot(xy_dict, output_base, obs_name, obs_range,
     hist_range=(obs_range[1],obs_range[2])
     
     # Upper plot
-    add_derivative_plot(ax_up, TGC_name, xy_dict, n_bins, hist_range, dev_scale)
+    add_TGC_derivative_plot(ax_up, TGC_name, xy_dict, n_bins, hist_range, dev_scale)
     
-    ax_up.set_ylabel(r"$\frac{{1}}{{\sigma^{{bin}}}}\frac{{d\sigma^{{bin}}}}{{d {}}}$".format(TGC_name_map[TGC_name]), fontsize=30)
+    ax_up.set_ylabel(r"$y=\frac{{1}}{{\sigma^{{bin}}}}\frac{{d\sigma^{{bin}}}}{{d {}}}$".format(TGC_name_map[TGC_name]), fontsize=30)
     ax_up.set_xlim(hist_range)
 
-    ax_up.legend(fontsize=16, title="${}$, $\delta_{{cut}}={}$".format(process_str, dev_scale, cut_dev), title_fontsize=16)
+    ax_up.legend(fontsize=16, title="${}$, $\delta_{{cut}}={}$".format(process_str, cut_dev), title_fontsize=16)
     
     # Lower plot
-    add_diff_plot(ax_down, TGC_name, xy_dict, n_bins, hist_range, dev_scale)  
+    add_TGC_diff_plot(ax_down, TGC_name, xy_dict, n_bins, hist_range, dev_scale)  
     ax_down.set_ylabel(r"$y_{cut}-y_{nocut}$")
     ax_down.set_xlabel(obs_name)
     
@@ -105,6 +148,34 @@ def create_reweighting_plot(xy_dict, output_base, obs_name, obs_range,
       ISH.create_dir(format_dir)
       fig.savefig("{}/{}_{}_TGCMuAccCorr.{}".format(format_dir, obs_name, 
                                                     TGC_name, format))
+
+    plt.close(fig)
+    
+  for cut_name in ["c_shift", "w_shift"]:
+    fig, axs = plt.subplots(2, 1, sharex=True, tight_layout=True, figsize=(10,10))
+    ax_up, ax_down = axs
+    
+    n_bins = obs_range[0]
+    hist_range=(obs_range[1],obs_range[2])
+    
+    # Upper plot
+    add_cut_derivative_plot(ax_up, cut_name, xy_dict, n_bins, hist_range, dev_scale)
+    
+    ax_up.set_ylabel(r"$y=\frac{{1}}{{\sigma^{{bin}}}}\frac{{d\sigma^{{bin}}}}{{d {}}}$".format(cut_name_map[cut_name]), fontsize=30)
+    ax_up.set_xlim(hist_range)
+
+    ax_up.legend(fontsize=16, title="${}$, $\delta_{{TGC}}={}$".format(process_str, dev_scale), title_fontsize=16)
+    
+    # Lower plot
+    add_cut_diff_plot(ax_down, cut_name, xy_dict, n_bins, hist_range, dev_scale)  
+    ax_down.set_ylabel(r"$y_{TGC}-y_{SM}$")
+    ax_down.set_xlabel(obs_name)
+    
+    for format in output_formats:
+      format_dir = "{}/{}".format(output_base,format)
+      ISH.create_dir(format_dir)
+      fig.savefig("{}/{}_{}_TGCMuAccCorr.{}".format(format_dir, obs_name, 
+                                                    cut_name, format))
 
     plt.close(fig)
 
@@ -124,8 +195,8 @@ def check_reweighting(root_file, tgc_config_path, tgc_point_path, output_dir,
   mu_acc_rdf_dict = {
     "no cut": mu_rdf,
     "cut": mu_rdf.Filter(SMA.get_costh_cut(cut_val, 0, 0, costh_branch)),
-    "c shift": mu_rdf.Filter(SMA.get_costh_cut(cut_val, cut_dev, 0, costh_branch)),
-    "w shift": mu_rdf.Filter(SMA.get_costh_cut(cut_val, 0, cut_dev, costh_branch))
+    "c_shift": mu_rdf.Filter(SMA.get_costh_cut(cut_val, cut_dev, 0, costh_branch)),
+    "w_shift": mu_rdf.Filter(SMA.get_costh_cut(cut_val, 0, cut_dev, costh_branch))
   }
   
   # TGC related setup
@@ -175,8 +246,8 @@ def check_reweighting(root_file, tgc_config_path, tgc_point_path, output_dir,
       xy_dict[cut_name] = {}
       for point, hist in cut_dict.items():
         xy_dict[cut_name][point] = PRHH.TH1_to_arrays(hist) 
-    create_reweighting_plot(xy_dict, output_base, obs_name, obs_range, 
-                            dev_scale, cut_dev, process_str)
+    create_reweighting_plots(xy_dict, output_base, obs_name, obs_range, 
+                             dev_scale, cut_dev, process_str)
       
 def main():
   """ Create check plots that investigate how the weights in the given ROOT file
